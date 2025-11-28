d7b390d1327e69471c8bc9700f202efd7c9247c1
gregb26 <gregoire.barrette@umontreal.ca>
2025-11-28 16:55:21 -0500

    added a function to generate a MP grid

--- src/electron_defect_interaction/utils/lattice.py
+++ src/electron_defect_interaction/utils/lattice.py
@@ -30,3 +30,38 @@ def red_to_cart(x_red, X):
     x = np.einsum('...j,ij->...i', x_red, X, optimize=True)
 
     return x
+
+def monkhorst_pack_grid(ngkpt, signed=True):
+    """
+    Generates a uniform (no symmetry reduction) Monkhorst-Pack kpoint grid in reduced coordinates from the tuple ngkpt. Returns the same grid
+    as Abinit if signed=True
+    Inputs:
+        ngkpt: tuple (N1, N2, N3) of ints
+            Number of kpoints in each direction
+        signed: Bool:
+            If True, fold [0,1) -> [-0.5, 0.5). Signed convention Abinit uses. Default is True.
+    Returns:
+        k_grid: (nk, 3) array of floats
+            kpoint grid in reduced coordinates
+    TODO: implement shift
+    """
+
+    N1, N2, N3 = ngkpt
+    N = np.prod(ngkpt)
+    k_grid = np.zeros((N,3), dtype=float)
+    
+    ik = 0
+    for i3 in range(N3):
+        k3 = i3 / N3
+        for i2 in range(N2):
+            k2 = i2 / N2
+            for i1 in range(N1):
+                k1 = i1 / N1
+                k_grid[ik, :] = (k1, k2, k3)
+                ik += 1
+    if signed:
+        k_grid = ((k_grid + 0.5) % 1.0) - 0.5 # fold [0, 1) -> [-0.5, 0.5)
+
+    return k_grid
+
+
